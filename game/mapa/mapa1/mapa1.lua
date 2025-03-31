694ecdf57680d2a4c14c0a36524f1d557e06e3a1
Filipi_Biazoto <134516780+FilipiBiazoto@users.noreply.github.com>
2025-03-30 22:25:38 -0300

    Add files via upload

--- game/mapa/mapa1/mapa1.lua
+++ game/mapa/mapa1/mapa1.lua
@@ -1,44 +1,60 @@
-local Objeto = require("mapa.mapa1.objeto") 
+local Objeto = require("mapa.library.objeto") 
+local Agua = require("mapa.library.agua") 
 local mapa1 = {}
-local config = require("mapa.mapa1.config")
-local debug = require("mapa.mapa1.debug")
+local config = require("mapa.generator.config")
+local debug = require("debug.debug")
 local Player = require("player.player")
 
 local player
 local objetosMapa1 = {}
+local aguasMapa1 = {}
 
 function mapa1.load(gameState)
     mapa1.gameState = gameState
     mapa1.debugAtivo = false
     config.load()
-    debug.load()
-
-    player = Player.new(100, 100)
+    debug.load() 
+    
+    player = Player.new(1100, 100)
+    
     for _, objeto in ipairs(config.objetos) do
         table.insert(objetosMapa1, Objeto.new(objeto.x, objeto.y, objeto.cor))
     end
-
+    
+    for _, agua in ipairs(config.aguas) do
+        table.insert(aguasMapa1, Agua.new(agua.x, agua.y, agua.cor))
+    end
 end
 
+
 function mapa1.draw()
-    config.draw()
+    --config.draw()
 
     for _, objeto in ipairs(objetosMapa1) do
         objeto:draw()
     end
+    
+    for _, agua in ipairs(aguasMapa1) do
+        agua:draw()
+    end
 
-    if player.visible then
+    if player and player.visible then
         player:draw()
     end
 
-    if mapa1.debugAtivo then
-        debug.draw()
-    end
+    -- Passando o jogador para exibir a velocidadeY no debug
+    debug.draw(mapa1.debugAtivo, player)
 end
 
+
+
 function mapa1.update(dt)
-    player:update()
-    player:handleCollisions(objetosMapa1)
+    if player and player.visible then
+        player:update(dt)  -- Passar 'dt' para o método update
+        player:handleCollisions(objetosMapa1)
+        player:handleCollisions2(aguasMapa1)
+        player:handleProjectileCollisions(objetosMapa1)  -- Verifica a colisão dos projéteis com objetos
+    end
 end
 
 function mapa1.keypressed(key)
@@ -49,19 +65,82 @@ end
 
 function checkCollision(player, objeto)
     return player.x < objeto.x + objeto.width and
-           player.x + 100 > objeto.x and
+           player.x + 32 > objeto.x and
            player.y < objeto.y + objeto.height and
-           player.y + 40 > objeto.y
+           player.y + 32 > objeto.y
 end
 
 function Player:handleCollisions(objetos)
     for _, objeto in ipairs(objetos) do
         if checkCollision(self, objeto) then
-            self.y = objeto.y - 40
-            self.velocidadeY = 0 
+            local overlapLeft = self.x + 32 - objeto.x
+            local overlapRight = objeto.x + objeto.width - self.x
+            local overlapTop = self.y + 32 - objeto.y
+            local overlapBottom = objeto.y + objeto.height - self.y
+
+            local minOverlap = math.min(overlapLeft, overlapRight, overlapTop, overlapBottom)
+
+            if minOverlap == overlapLeft then
+                self.x = objeto.x - 32
+            elseif minOverlap == overlapRight then
+                self.x = objeto.x + objeto.width 
+            elseif minOverlap == overlapTop then
+                self.y = objeto.y - 32 
+                self.velocidadeY = 0
+            elseif minOverlap == overlapBottom then
+                self.y = objeto.y + objeto.height 
+            end
         end
     end
 end
+
+
+function checkCollisionAgua(player, agua)
+    return player.x < agua.x + agua.width and
+           player.x + 32 > agua.x and
+           player.y < agua.y + agua.height and
+           player.y + 32 > agua.y
+end
+
+function Player:handleCollisions2(aguas)
+    for _, agua in ipairs(aguas) do
+        if checkCollisionAgua(self, agua) then
+            player.visible = false
+        end
+    end
+end
+
+function checkCollisionProjectile(proj, objeto)
+    return proj.x < objeto.x + objeto.width and
+           proj.x + 5 > objeto.x and  -- O raio do projétil é 5, então 10 é o diâmetro.
+           proj.y < objeto.y + objeto.height and
+           proj.y + 5 > objeto.y  -- O raio do projétil é 5, então 10 é o diâmetro.
+end
+
+function Player:handleProjectileCollisions(objetos)
+    for i, proj in ipairs(self.projectiles) do
+        for _, objeto in ipairs(objetos) do
+            if checkCollisionProjectile(proj, objeto) then
+                -- Remover o projétil da lista
+                table.remove(self.projectiles, i)
+
+                -- Destruir objetos dentro de um raio de 100px da colisão
+                for j = #objetos, 1, -1 do  -- Iterar ao contrário para não pular elementos ao remover
+                    local objeto2 = objetos[j]
+                    local dist = math.sqrt((objeto2.x - proj.x)^2 + (objeto2.y - proj.y)^2)
+                    if dist <= 100 then  -- Verifica se a distância é menor ou igual a 100px
+                        table.remove(objetos, j)  -- Remove o objeto dentro do raio
+                        objeto2 = nil 
+                    end
+                end
+
+                break  -- Já encontrou uma colisão, então não precisa continuar verificando
+            end
+        end
+    end
+end
+
+
 
 
 return mapa1
--- game/player/player.lua
+++ game/player/player.lua
@@ -8,25 +8,125 @@ function Player.new(x, y)
     self.speed = 5
     self.visible = true
     self.velocidadeY = 0
-    self.gravidade = 0.5
+    self.gravidade = 0.2
+    self.pulando = false
+    self.alturaMaximaDoPulo = -3.8
+    self.projectiles = {} -- Lista para armazenar os projéteis
+    self.anguloTiro = 0 -- Ângulo inicial da seta
+    self.raioMira = 50 -- Raio da área de tiro
+    self.lastShotTime = 0 -- Tempo do último disparo
+    self.shootCooldown = 1 -- Tempo de espera entre disparos (em segundos)
+    
+    self.forcaTiro = 0
+    self.maxForca = 50 -- Força máxima do tiro
+
     return self
 end
 
-function Player:update()
+function Player:newProjectile(startX, startY, angle, speed)
+    local projectile = {
+        x = startX,
+        y = startY,
+        speed = speed,
+        angle = angle,
+        velocidadeY = 0, -- Velocidade vertical inicial do projétil
+    }
+    table.insert(self.projectiles, projectile)
+end
+
+function Player:update(dt)
+    -- Movimentação do jogador
     if love.keyboard.isDown('a') then
         self.x = self.x - self.speed
     elseif love.keyboard.isDown('d') then
         self.x = self.x + self.speed
     end
 
+    if love.keyboard.isDown('space') and self.velocidadeY == 0 then
+        self.velocidadeY = self.alturaMaximaDoPulo
+        self.pulando = true
+    end
+
+    -- Movimentação da mira
+    if love.keyboard.isDown('w') then
+        self.anguloTiro = self.anguloTiro - 0.05
+    elseif love.keyboard.isDown('s') then
+        self.anguloTiro = self.anguloTiro + 0.05
+    end
+
+    -- Acumula a força enquanto a tecla 'f' é pressionada
+    if love.keyboard.isDown('f') then
+        if self.forcaTiro < self.maxForca then
+            self.forcaTiro = self.forcaTiro + 0.5 -- Aumenta a força aos poucos
+        end
+    elseif self.forcaTiro > 0 then
+        self:shoot() -- Dispara quando a tecla 'f' é solta
+        self.forcaTiro = 0 -- Reseta a força
+    end
+
+    -- Aplica a gravidade ao jogador
     self.velocidadeY = self.velocidadeY + self.gravidade
     self.y = self.y + self.velocidadeY
+
+    if self.y >= love.graphics.getHeight() - 32 then
+        self.y = love.graphics.getHeight() - 32
+        self.velocidadeY = 0
+        self.pulando = false 
+    end
+
+    -- Atualiza os projéteis
+    for i, proj in ipairs(self.projectiles) do
+        -- Aplique a gravidade no projétil
+        proj.velocidadeY = proj.velocidadeY + self.gravidade
+        proj.y = proj.y + proj.velocidadeY
+        
+        -- Movimenta o projétil na direção do ângulo
+        proj.x = proj.x + proj.speed * math.cos(proj.angle)
+        proj.y = proj.y + proj.speed * math.sin(proj.angle)
+    end
+end
+
+function Player:shoot()
+    local startX = self.x + 16 + self.raioMira * math.cos(self.anguloTiro)
+    local startY = self.y + 16 + self.raioMira * math.sin(self.anguloTiro)
+    self:newProjectile(startX, startY, self.anguloTiro, self.forcaTiro)
 end
 
 function Player:draw()
     love.graphics.setColor(1, 0, 0)
-    love.graphics.rectangle('fill', self.x, self.y, 100, 40)
+    love.graphics.rectangle('fill', self.x, self.y, 32, 32)
     love.graphics.setColor(1, 1, 1)
+    
+    -- Aumenta a espessura da linha de mira
+    love.graphics.setLineWidth(4)  -- Ajuste o valor conforme necessário para a espessura
+
+    -- Desenha a seta de mira
+    local miraX = self.x + 16 + self.raioMira * math.cos(self.anguloTiro)
+    local miraY = self.y + 16 + self.raioMira * math.sin(self.anguloTiro)
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.line(self.x + 16, self.y + 16, miraX, miraY)
+    love.graphics.circle('fill', miraX, miraY, 5)
+    
+    -- Desenha os projéteis
+    for _, proj in ipairs(self.projectiles) do
+        love.graphics.setColor(1, 1, 0)
+        love.graphics.circle('fill', proj.x, proj.y, 5)
+    end
+    
+    love.graphics.setColor(1, 1, 1)
+
+    -- Desenha a barra de força acompanhando a linha de mira com a espessura aumentada
+    local barraForcaX = self.x + 16
+    local barraForcaY = self.y + 16
+    local barraForcaFimX = barraForcaX + self.forcaTiro * math.cos(self.anguloTiro)
+    local barraForcaFimY = barraForcaY + self.forcaTiro * math.sin(self.anguloTiro)
+
+    love.graphics.setColor(0, 1, 0)
+    love.graphics.line(barraForcaX, barraForcaY, barraForcaFimX, barraForcaFimY)
+
+    -- Reseta a espessura da linha para o padrão após desenhar
+    love.graphics.setLineWidth(4)  -- Resetando para o valor padrão
 end
+
 
 return Player
