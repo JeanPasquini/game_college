701073f2751ca053b58213620034698c2637ab55
Filipi_Biazoto <134516780+FilipiBiazoto@users.noreply.github.com>
2025-03-30 22:24:38 -0300

    Add files via upload

---
+++ game_college-develop-menu-principal/game/mapa/library/agua.lua
@@ -0,0 +1,21 @@
+local Agua = {}
+Agua.__index = Agua
+
+function Agua.new(x, y, cor)
+    local self = setmetatable({}, Agua)
+    self.x = x
+    self.y = y
+    self.cor = cor
+    self.width = 16
+    self.height = 16
+    return self
+end
+
+function Agua:draw()
+    local px = self.x
+    local py = self.y
+    love.graphics.setColor(self.cor[1], self.cor[2], self.cor[3])
+    love.graphics.rectangle("fill", px, py, self.width, self.height)
+end
+
+return Agua
